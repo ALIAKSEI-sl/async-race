1c643f0310274506be8c6dfaa75206c02d3fa339
Aliaksei <sladkiy.aleksey@gmail.com>
2024-04-29 16:32:31 +0200

    feat: add winners table

---
+++ src/app/pages/winners-page/components/winners-table/winners-table.component.ts
@@ -0,0 +1,43 @@
+import {
+	ChangeDetectionStrategy,
+	Component,
+	computed,
+	inject
+} from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
+import { WinnersKeeperService } from '../../../../services/winners.keeper.service';
+import { AngularSvgIconModule } from 'angular-svg-icon';
+import { SvgIconComponent } from 'angular-svg-icon';
+import { SortingLabel, Sort } from '../../../../models/winners.model';
+import { SORTING_LADLE } from '../../../../constants/sorting';
+import { columns } from '../../../../constants/winners-table';
+
+@Component({
+	selector: 'race-winners-table',
+	standalone: true,
+	imports: [MatTableModule, AngularSvgIconModule, SvgIconComponent],
+	templateUrl: './winners-table.component.html',
+	styleUrl: './winners-table.component.scss',
+	changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class WinnersTableComponent {
+	private readonly label = SORTING_LADLE;
+	public readonly displayedColumns = columns;
+
+	private readonly keeperService = inject(WinnersKeeperService);
+
+	public readonly dataSource = this.keeperService.winners;
+
+	public sortingLabel = computed<SortingLabel>(() => {
+		const { order, sort } = this.keeperService.orderWinners();
+
+		return { ...this.label, [sort]: order === 'ASC' ? '&#8595;' : '&#8593;' };
+	});
+
+	public sort(tag: Sort): void {
+		const { sort } = this.keeperService.orderWinners();
+
+		if (tag === sort) this.keeperService.changeOrder();
+		else this.keeperService.changeSorting(tag);
+	}
+}
--- src/app/pages/winners-page/winners-page.component.ts
+++ src/app/pages/winners-page/winners-page.component.ts
@@ -1,11 +1,28 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { WinnersTableComponent } from './components/winners-table/winners-table.component';
+import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
+import { WinnersKeeperService } from '../../services/winners.keeper.service';
+import { LIMIT_WINNERS_PAGE } from '../../constants/limits';
 
 @Component({
 	selector: 'race-winners-page',
 	standalone: true,
-	imports: [],
+	imports: [WinnersTableComponent, MatPaginatorModule],
 	templateUrl: './winners-page.component.html',
 	styleUrl: './winners-page.component.scss',
 	changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class WinnersPageComponent {}
+export class WinnersPageComponent {
+	public readonly pageSize = LIMIT_WINNERS_PAGE;
+
+	private readonly keeperService = inject(WinnersKeeperService);
+
+	public readonly page = this.keeperService.page;
+	public readonly totalCountWinner = this.keeperService.totalCountWinner;
+
+	public changePage(event: PageEvent): void {
+		const page = event.pageIndex + 1;
+
+		this.keeperService.page.set(page);
+	}
+}
